718590cfb8473667b97a26a3983a2894bbd7981a
ToraNova <chia_jason96@live.com>
2019-05-10 14:28:19 +0800

    allow external imports from __init__.py addition

---
+++ __init__.py
@@ -0,0 +1,13 @@
+# This is the module's main import file
+# include modules that are wrapped here
+# sometimes it is stupid since there may
+# be chances that the module is the same
+# as the library name, causing someting
+# like lib.lib imports, but that is all
+# for the sake of future extensibility
+
+# to use the import, from other external file,
+# if this directory is included in their path
+# then do 
+# import pyplasma.pyplasma
+import pyplasma
---
+++ src/p_run.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+import sys
+sys.path.append('..')
+
+import pyplasma.pyplasma as pyplasma
+
+if __name__ == "__main__":
+
+    print(pyplasma)
+    pyplasma.pputil_test()
+
