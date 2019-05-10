35c0d054bc5ec3368f6fc399675cd5a5e91e94a4
ToraNova <chia_jason96@live.com>
2019-05-10 11:46:25 +0800

    working plasma version

--- include/pputil.h
+++ include/pputil.h
@@ -7,10 +7,18 @@
 #ifndef PPUTIL_H
 #define PPUTIL_H
 
+#include <stdint.h>
+#include <stddef.h>
 
 /*
  * Test function to ensure library usability
  */
 void pputil_test();
+
+double *pputil_ridge(
+	double *Dmat, size_t xrowsz, size_t xcolsz,
+	double *Tvct, size_t yelmsz, size_t ydimsz,
+	double lambda
+);
 
 #endif
--- src/pputil_testrun.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/python3
-
-# Test function for the pputil classes
-# They involve file reading
-# and simple linear algebraic methods
-# such as inversion
